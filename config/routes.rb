7a31a49e48a2fb610c1c7cabb0266a72531b3c9f
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-10-07 14:51:36 -0600

    going to town on the clf template and css to make it match the pwgsc site more

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.english 'english', :controller => 'language_switcher', :action => 'english'
-  map.french 'french', :controller => 'language_switcher', :action => 'french'
+  map.french 'francais', :controller => 'language_switcher', :action => 'french'
+  map.contact 'contact-us', :controller => 'application', :action => 'contact'
+  map.contact 'contactez-nous', :controller => 'application', :action => 'contact'
+  map.help 'help', :controller => 'application', :action => 'help'
+  map.help 'aide', :controller => 'application', :action => 'help'
 end
